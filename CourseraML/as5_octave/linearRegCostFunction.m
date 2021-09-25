6a28637ce90a2080bda60f83199f56a6f3d55dbb
shavit-es <71497140+shavit-es@users.noreply.github.com>
2021-09-25 15:00:41 +0900

    theta 부분 수정
    theta를 포함해서 계산한 후 grad를 바꿔주면 부정확한 값이 나오게 됨
    따라서 처음부터 0으로 설정하여 계산

--- CourseraML/as5_octave/linearRegCostFunction.m
+++ CourseraML/as5_octave/linearRegCostFunction.m
@@ -18,11 +18,14 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
-J= sum((X*theta-y).^2)/(2*m) + (sum(theta(2:end).^2))*(lambda/(2*m));
 
-grad = sum((X*theta-y).*X)/m + (theta)'*(lambda/m);
-j0grad=(sum(X*theta-y)*X)/m;
-grad(1) = j0grad(1);
+nowtheta = theta;
+nowtheta(1) = 0; 
+
+h=X*theta
+J= sum((h-y).^2)/(2*m) + sum(sum(theta(2:end,:).^2))*(lambda/(2*m))
+
+grad = sum((X*theta-y).*X)/m + (nowtheta)'*(lambda/m);
 
 
 
